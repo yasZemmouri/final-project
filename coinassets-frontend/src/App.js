430b7cd445d5b8ae2f3b6ef9e0e7b5b605732c96
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-03-04 23:55:38 -0500

    make exp

--- coinassets-frontend/src/App.js
+++ coinassets-frontend/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
+//Style
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/' element={<Home/>}/>
+        <Route path='*' element={<NotFound/>}/>
+
+      </Routes>
+    </Router>
   );
 }
 
