b640c8e278a1131f83a972d3f28d367311423cf0
Yassine Zemmouri <yzemmouri.eng@gmail.com>
2022-03-08 05:56:49 -0500

    search form

--- coinassets-frontend/src/pages/Home.js
+++ coinassets-frontend/src/pages/Home.js
@@ -2,17 +2,26 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 // import CoinData from '../components/CoinData';
 import CoinsTable from '../components/CoinsTable';
-// import LogoutButton from '../components/LoginButton';
-import LoginButton from '../components/LoginButton';
-import LogoutButton from '../components/LogoutButton';
-import { Profile } from '../components/Profile';
+
+
+
 
 export default function Home() {
+  const [query, setQuery]=useState('');
+  //are you sure it's an array
+  const [result, setResult]= useState([])
   const [coinsList, setCoinsList]=useState([]);
   console.log(coinsList);
   useEffect(()=>{
     fetchData()
   }, []);
+  const handleChange=(e)=>{
+    setQuery(e.target.value)
+  }
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    console.log('searching for ' + query);
+  }
   const fetchData = async ()=>{
     const res = await axios.get('https://api.coinstats.app/public/v1/coins?skip=0&limit=100&curreny=USD')
     if (!res.erros) setCoinsList(res.data.coins) 
@@ -23,6 +32,13 @@ export default function Home() {
     <main>
       <div className="container">
         <h1>Dashboard</h1>
+        <form className="d-flex m-2 mx-auto" onSubmit={handleSubmit}>
+          <input type="search" id="query" className="form-control me-2" placeholder="Enter coin's name..."
+          aria-label="Search"
+          onChange={handleChange}
+          value={query}/>
+          <button id="busqueda" className = "btn btn-outline-primary" type="submit">Search</button>
+        </form>
         <CoinsTable coinsList={coinsList}/>
       </div>
     </main>
